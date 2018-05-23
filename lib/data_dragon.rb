f73f964e77b2450a867ef77fad5058b685ec7c8e
Brenton "B-Train" Fletcher <i@bloople.net>
2018-05-23 16:22:39 +1000

    Remove unused code

--- lib/data_dragon.rb
+++ lib/data_dragon.rb
@@ -10,12 +10,4 @@ module DataDragon
   def self.data_pack_path
     Build.data_path + "pack.tgz"
   end
-
-  def self.item_path
-    data_unpacked_path + version_path.read + "data" + "en_AU" + "item.json"
-  end
-
-  def self.item_image_path(id)
-    data_unpacked_path + version_path.read + "img" + "item" + "#{id}.png"
-  end
 end
